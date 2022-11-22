ce88eaaa4c209da8c03c6f302a11e539c89c3c74
uewquewqueqwue <91084267+uewquewqueqwue@users.noreply.github.com>
2022-11-22 23:53:30 +0300

    changing choice to a tabular view, good luck fixing the design :)

--- abzagencytask/employee/models.py
+++ abzagencytask/employee/models.py
@@ -1,15 +1,8 @@
 from django.db import models
+
 # from django.urls import reverse
 
 # Create your models here.
-
-POSITIONS = (
-    (1, "chief"),
-    (2, "shareholder"),
-    (3, "president"),
-    (4, "manager"),
-    (5, "employee"),
-)
 
 
 class Employee(models.Model):
@@ -18,7 +11,14 @@ class Employee(models.Model):
     name = models.CharField(max_length=50)
     surname = models.CharField(max_length=50)
     patronymic = models.CharField(max_length=50, null=True, blank=True)
-    position = models.CharField(max_length=50, choices=POSITIONS, default=3, null=True, blank=True)
+    position = models.ForeignKey(
+        "Position",
+        max_length=50,
+        default=5,
+        null=True,
+        blank=True,
+        on_delete=models.PROTECT,
+    )
     date_employ = models.DateTimeField(auto_now_add=True)
     salary = models.IntegerField(null=True, blank=True)
     chief = models.ForeignKey("self", null=True, blank=True, on_delete=models.PROTECT)
@@ -30,3 +30,12 @@ class Employee(models.Model):
     # not needed yet
     # def get_absolute_url(self) -> str:
     #     return reverse("employee", kwargs={"employee_id": self.pk})
+
+
+class Position(models.Model):
+    """There should be your comment here"""
+
+    name = models.CharField(max_length=50)
+
+    def __str__(self) -> str:
+        return str(self.name)
